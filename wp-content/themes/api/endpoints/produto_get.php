b48cecb6d45a80f93818966ce806b4db1b984439
DeivisonSantana <deivisonssouza@hotmail.com>
2022-12-13 08:18:16 -0400

    refactor(doc) produto_get * consertado alguns erros de escrita no código

--- wp-content/themes/api/endpoints/produto_get.php
+++ wp-content/themes/api/endpoints/produto_get.php
@@ -1,5 +1,6 @@
 <?php
-function produto_scheme($slug){
+
+function produto_scheme($slug) {
   $post_id = get_produto_id_by_slug($slug);
   if($post_id) {
     $post_meta = get_post_meta($post_id);
@@ -7,7 +8,7 @@ function produto_scheme($slug){
     $images = get_attached_media('image', $post_id);
     $images_array = null;
 
-    if($images){
+    if($images) {
       $images_array = array();
       foreach($images as $key => $value) {
         $images_array[] = array(
@@ -18,7 +19,7 @@ function produto_scheme($slug){
     }
 
     $response = array(
-      'id' => $slug,
+      "id" => $slug, 
       "fotos" => $images_array,
       "nome" => $post_meta['nome'][0],
       "preco" => $post_meta['preco'][0],
@@ -28,36 +29,36 @@ function produto_scheme($slug){
     );
 
   } else {
-    $response = new WP_error('nao existe', 'Produto nao encontrado', array('status' => 404));
+    $response = new WP_Error('naoexiste', 'Produto não encontrado.', array('status' => 404));
   }
   return $response;
 }
 
 function api_produto_get($request) {
-  $response = produto_scheme([$request["slug"]])
+  $response = produto_scheme($request["slug"]);
   return rest_ensure_response($response);
 }
 
 function registrar_api_produto_get() {
-  register_rest_route('api', '/produto/(?P<slug>[-\W]+)', array(
+  register_rest_route('api', '/produto/(?P<slug>[-\w]+)', array(
     array(
       'methods' => WP_REST_Server::READABLE,
       'callback' => 'api_produto_get',
     ),
   ));
 }
-
 add_action('rest_api_init', 'registrar_api_produto_get');
 
-//API PRODUTOS
-function api_produtos_get($request){
+// API PRODUTOS
+function api_produtos_get($request) {
 
   $q = sanitize_text_field($request['q']) ?: '';
   $_page = sanitize_text_field($request['_page']) ?: 0;
   $_limit = sanitize_text_field($request['_limit']) ?: 9;
   $usuario_id = sanitize_text_field($request['usuario_id']);
+
   $usuario_id_query = null;
-  if($usuario_id){
+  if($usuario_id) {
     $usuario_id_query = array(
       'key' => 'usuario_id',
       'value' => $usuario_id,
@@ -73,12 +74,12 @@ function api_produtos_get($request){
 
   $query = array(
     'post_type' => 'produto',
-    'post_per_page' => $_limit,
+    'posts_per_page' => $_limit,
     'paged' => $_page,
     's' => $q,
     'meta_query' => array(
       $usuario_id_query,
-      $vendido
+      $vendido,
     )
   );
 
@@ -105,6 +106,7 @@ function registrar_api_produtos_get() {
     ),
   ));
 }
-
 add_action('rest_api_init', 'registrar_api_produtos_get');
+
+
 ?>
